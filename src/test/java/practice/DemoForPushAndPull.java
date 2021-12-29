30cf4afa5dbee8b4a3b3acf5d0d5b934b78e0f83
91935 <91935@DESKTOP-83QCFHP>
2021-12-29 17:56:24 +0530

    added method to demoforpushandpull

---
+++ src/test/java/practice/DemoForPushAndPull.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class DemoForPushAndPull {
+	
+	public void createCustomer() {
+		System.out.println("Create customer");
+	}
+	
+	public void deleteCustomer() {
+		System.out.println("delete customer");
+	}
+
+}
