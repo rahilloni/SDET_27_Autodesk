61a925d3ee075a671f09edd9570e0ac767994c9f
91935 <91935@DESKTOP-83QCFHP>
2021-12-29 17:50:28 +0530

    added method for DemoForPush

---
+++ src/test/java/practice/DemoForPush.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class DemoForPush {
+	
+	public void editCutomer() {
+		System.out.println("edit customer");
+	}
+	
+	public void deleteCustomer() {
+		System.out.println("deleteCustomer");
+	}
+
+}
